10324500496fb021a77c7f917d113f1690f95887
Yun9517 <38489388+Yun9517@users.noreply.github.com>
2025-07-07 23:50:39 +0800

    Update: All period content in About section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import PhotoGallery from './components/PhotoGallery';
@@ -8,6 +11,13 @@ import VideoGallery from './components/VideoGallery';
 import Footer from './components/Footer';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // animation duration
+      once: true, // whether animation should happen only once - while scrolling down
+    });
+  }, []);
+
   return (
     <div className="App d-flex flex-column min-vh-100">
       <Header />
--- src/components/About.js
+++ src/components/About.js
@@ -3,28 +3,63 @@ import { Container, Row, Col, Card, Image } from 'react-bootstrap';
 
 const About = () => {
   return (
-    <Container id="home" className="my-5">
-      <Row className="align-items-center">
-        <Col md={4} className="text-center mb-4 mb-md-0">
-          <Image src={`${process.env.PUBLIC_URL}/yeomsebin_about.jpg`} roundedCircle fluid className="profile-image" style={{ width: '300px', height: '300px', objectFit: 'cover', border: '5px solid #007bff' }} />
-        </Col>
-        <Col md={8}>
-          <h2>關於 廉世彬 (Yeom Se-been)</h2>
-          <p>
-            廉世彬（韓語：염세빈／廉世彬 Yeom Se-bin），出生於2002年4月23日，是韓國的啦啦隊員。她以其充滿活力的表演和迷人的個性而受到粉絲的喜愛。她曾效力於韓國職棒起亞虎和韓國排球聯賽水原韓國電力빅스톰，目前是中華職棒樂天桃猿啦啦隊「Rakuten Girls」的一員。
-          </p>
-          <Card bg="light" className="mt-3">
-            <Card.Body>
-              <Card.Text>
-                <strong>姓名：</strong> 廉世彬 (Yeom Se-been)<br/>
-                <strong>生日：</strong> 2002年4月23日<br/>
-                <strong>現屬隊伍：</strong> Rakuten Girls (樂天女孩)
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <div className="about-sections">
+      {/* 2022 新人時期 */}
+      <section className="about-section section-2022 py-5">
+        <Container>
+          <Row className="align-items-center">
+            <Col md={6} data-aos="fade-right">
+              <Image src={`${process.env.PUBLIC_URL}/yeomsebeen_2022_volleyball_01.png`} fluid rounded className="mb-3" />
+              <Image src={`${process.env.PUBLIC_URL}/yeomsebeen_2022_volleyball_02.jpg`} fluid rounded />
+            </Col>
+            <Col md={6} data-aos="fade-left">
+              <h2 className="text-white">2022 新人時期</h2>
+              <p className="text-white">
+                2022年12月3日正式以職業啦啦隊員出道的廉世彬，當時先參與職業男子排球 KEPCO Vixtorm Volleyball Team 以及職業女子排球 Hana Bank Women's Basketball Team的應援工作，當時受到的關注度還不高
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* 2023-2024 起亞虎時期 */}
+      <section className="about-section section-2023 py-5">
+        <Container>
+          <Row className="align-items-center flex-md-row-reverse">
+            <Col md={6} data-aos="fade-left">
+              <Image src={`${process.env.PUBLIC_URL}/yeomsebin_2023_kia_01.jpg`} fluid rounded className="mb-3" />
+              <Image src={`${process.env.PUBLIC_URL}/yeomsebin_2023_kia_02.jpg`} fluid rounded className="mb-3" />
+              <Image src={`${process.env.PUBLIC_URL}/yeomsebin_2023_kia_03.jpg`} fluid rounded />
+            </Col>
+            <Col md={6} data-aos="fade-right">
+              <h2 className="text-white">2023-2024 起亞虎時期</h2>
+              <p className="text-white">
+                2023年正式加入起亞虎隊應援團，而這兩年她也分別和邊荷律以及李珠珢成為隊友。2024年在韓國的網路論《Dctrend》上舉辦了一個職業啦啦隊人氣投票，而她最終以12,817票擊敗權熙媛、李珠珢以及李多慧拿下冠軍，也被封為2024年度職業啦啦隊女王，人氣也是在這時期快速上升。除了職業啦啦隊工作外，2024年11月12日廉世彬以藝名Snooze發表個人參與創作的單曲새벽감성，顯示出她多才多藝的面向。
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* 2025 現況，樂天桃猿與 NV 恐龍兩地應援時期 */}
+      <section className="about-section section-2025 py-5">
+        <Container>
+          <Row className="align-items-center">
+            <Col md={6} data-aos="fade-right">
+              <Image src={`${process.env.PUBLIC_URL}/yeomsebin_2025_rakuten_01.jpg`} fluid rounded className="mb-3" />
+              <Image src={`${process.env.PUBLIC_URL}/yeomsebin_2025_rakuten_02.jpg`} fluid rounded className="mb-3" />
+              <Image src={`${process.env.PUBLIC_URL}/yeomsebin_2025_nc_01.jpg`} fluid rounded />
+            </Col>
+            <Col md={6} data-aos="fade-left">
+              <h2 className="text-white">2025現況</h2>
+              <p className="text-white">
+                在季初宣布加入NC恐龍隊，隨後在2月26日宣布加入台灣樂天桃猿隊，IG粉絲也在今年突破10萬，個人相關周邊商品總是銷售一空。在今年6月16日以Snooze名字發行個人第二張單曲Cherry Blooming，即將在7月19日中華職棒明星賽參與開幕表演，並且會在明星賽期間在球場內快閃店「好厝邊 Hasubeen」攜手知名餐飲品牌「饗食天堂」聯名推出韓式料理，首度圓夢當店長，販賣韓國美食啦啦隊女王廉世彬的故事還在繼續，帶給粉絲們驚喜
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </div>
   );
 };
 
