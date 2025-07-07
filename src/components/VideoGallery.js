a4f9cc5bce1af210003cdb0cb505e62560cb2e1f
Yun9517 <38489388+Yun9517@users.noreply.github.com>
2025-07-08 00:50:09 +0800

    Feature: Add latest and most popular videos to Video Gallery

--- src/components/VideoGallery.js
+++ src/components/VideoGallery.js
@@ -10,7 +10,7 @@ const VideoGallery = () => {
           <div className="ratio ratio-16x9">
             <iframe 
               src="https://www.youtube.com/embed/gMb8xHtg-VA" 
-              title="Yeom Se-bin Fancam 1"
+              title="固定影片 1"
               allowFullScreen
             ></iframe>
           </div>
@@ -19,7 +19,25 @@ const VideoGallery = () => {
           <div className="ratio ratio-16x9">
             <iframe 
               src="https://www.youtube.com/embed/0ViTu7kRdlM" 
-              title="Yeom Se-bin Fancam 2"
+              title="固定影片 2"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </Col>
+        <Col md={6} className="mb-4">
+          <div className="ratio ratio-16x9">
+            <iframe 
+              src="https://www.youtube.com/embed/YlSq4EGFrl0" 
+              title="最新影片"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </Col>
+        <Col md={6} className="mb-4">
+          <div className="ratio ratio-16x9">
+            <iframe 
+              src="https://www.youtube.com/embed/7xrOohoT6jg" 
+              title="最熱門影片"
               allowFullScreen
             ></iframe>
           </div>
