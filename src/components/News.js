16796a5443d7c0ecc9ffa23001cdf7273b238f9f
Yun9517 <38489388+Yun9517@users.noreply.github.com>
2025-07-08 14:46:02 +0800

    feat: Integrate Elfsight Instagram and Threads widgets into News      page

--- src/components/News.js
+++ src/components/News.js
@@ -11,22 +11,24 @@ const News = () => {
           
           <div className="my-4">
             <h2 className="text-white">Threads 動態</h2>
-            {/* 
-              這裡是預留給 Threads Widget 的區塊。
-              您可以從 Threads 的官方開發者文件或第三方服務 (如 Elfsight) 獲取嵌入代碼，
-              然後將其貼在此處。
-            */}
-            <p className="text-white-50">Threads 動態牆即將推出...</p>
+            {/* Elfsight Social Feed */}
+            <div dangerouslySetInnerHTML={{
+              __html: `
+                <script src="https://static.elfsight.com/platform/platform.js" async></script>
+                <div class="elfsight-app-84806fe0-be21-43f6-b389-27805bb9f9b0" data-elfsight-app-lazy></div>
+              `
+            }} />
           </div>
 
           <div className="my-4">
             <h2 className="text-white">Instagram 動態</h2>
-            {/* 
-              這裡是預留給 Instagram Widget 的區塊。
-              您可以從 Instagram 的官方開發者文件或第三方服務 (如 Elfsight) 獲取嵌入代碼，
-              然後將其貼在此處。
-            */}
-            <p className="text-white-50">Instagram 動態牆即將推出...</p>
+            {/* Elfsight Instagram Feed */}
+            <div dangerouslySetInnerHTML={{
+              __html: `
+                <script src="https://static.elfsight.com/platform/platform.js" async></script>
+                <div class="elfsight-app-f67c5a06-db6b-42cc-b17e-87e56c3dea9f" data-elfsight-app-lazy></div>
+              `
+            }} />
           </div>
 
         </Col>
