f61b441cf3223fdb6d9def2d5ca2b18a742a8c9c
Yun9517 <38489388+Yun9517@users.noreply.github.com>
2025-07-08 15:29:32 +0800

    Improve News page and the layout of wiget

--- src/components/News.js
+++ src/components/News.js
@@ -11,13 +11,14 @@ const News = () => {
           
           <div className="my-4">
             <h2 className="text-white">Threads 動態</h2>
-            {/* Elfsight Social Feed */}
-            <div dangerouslySetInnerHTML={{
-              __html: `
-                <script src="https://static.elfsight.com/platform/platform.js" async></script>
-                <div class="elfsight-app-84806fe0-be21-43f6-b389-27805bb9f9b0" data-elfsight-app-lazy></div>
-              `
-            }} />
+            <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+              {/* Elfsight Social Feed */}
+              <div dangerouslySetInnerHTML={{
+                __html: `
+                  <div class="elfsight-app-84806fe0-be21-43f6-b389-27805bb9f9b0" data-elfsight-app-lazy></div>
+                `
+              }} />
+            </div>
           </div>
 
           <div className="my-4">
@@ -25,7 +26,6 @@ const News = () => {
             {/* Elfsight Instagram Feed */}
             <div dangerouslySetInnerHTML={{
               __html: `
-                <script src="https://static.elfsight.com/platform/platform.js" async></script>
                 <div class="elfsight-app-f67c5a06-db6b-42cc-b17e-87e56c3dea9f" data-elfsight-app-lazy></div>
               `
             }} />
